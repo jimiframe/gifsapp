55b51f3af8d7507030274ea5e452a5dd6963b743
jisladev <jisladev@gmail.com>
2024-12-15 20:57:26 +0100

    feat(http-get): add GIF search functionality and HTTP client integration
    Introduced interfaces for GIF data models and integrated HttpClientModule for API communication. Updated GifsService to fetch GIFs from Giphy API based on search terms and maintain search history.

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,17 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Gif, SearchResponse} from "../interfaces/gifs.interfaces";
 
 @Injectable({  providedIn: 'root'} )
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = []
 
-  constructor() {}
+  private _tagsHistory: string[] = [];
+  private apiKey: string = 'QUDZICYb1eKbVNMHTGfNF87UGyUJb7Qx';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) {}
 
   get tagsHistory(): string[] {
     return [...this._tagsHistory];
@@ -14,6 +20,17 @@ export class GifsService {
   searchTags(tag: string): void {
     if ( tag.length === 0 ) return;
     this.organizeHistory( tag );
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', '10');
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe( resp  => {
+        this.gifList = resp.data;
+        console.log({gifs: this.gifList});
+      })
 
   }
 
